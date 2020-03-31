b22756c7adf161d0d210c6e113dd8aeff43b571d
feimaomiao <lcpmatthew@gmail.com>
2020-03-31 11:14:21 -0700

    Fixed problem of not finding package

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ with open('README.md', 'r') as fh:
 	    long_description=actual_description,
 	    long_description_content_type="text/markdown",
 	    url="https://github.com/feimaomiao/lazyloads",
-	    package_dir = {'':'lzl'},
-	    packages=setuptools.find_packages("lzl", exclude=['tests']),
+	    packages=setuptools.find_packages(exclude=['tests']),
 	    classifiers=[
 	        "Programming Language :: Python :: 3",
 	        "License :: OSI Approved :: MIT License",
