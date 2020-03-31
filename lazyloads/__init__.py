06f6803d18def3aad1a186eae8f926cbdc121269
feimaomiao <lcpmatthew@gmail.com>
2020-03-30 17:18:28 -0700

    modified import methods in init and added new functions

--- lazyloads/__init__.py
+++ lazyloads/__init__.py
@@ -1,20 +1,30 @@
-from copy import deepcopy as dc
+from copy import deepcopy as _dc
 import time
-from random import seed as randomseed, randrange as rrange
-randomseed(time.time()*rrange(1234,123456789))
-del randomseed, time,rrange
+from random import seed as _seed ,randrange as _randrange ,sample as _sample
+from random import shuffle as _shuffle, choice as _choice
+from itertools import product as _product, permutations as _permutations
+from itertools import combinations as _combinations,combinations_with_replacement as _combinations_with_replacement
+from hashlib import sha512 as _sha512
+from math import ceil as _mceil
+from string import ascii_letters as _ascii_chars, digits as _digs
+from re import search as _se
+_seed(time.time()*_randrange(1234,123456789))
+del time
 
+__all__ = ['lzlist','lzstr','lzdict','lzint','lzfloat']
+__author__  = 'Matthew Lam'
+__email__   = 'lcpmatthew@gmail.com'
+__version__ = '0.0.1'   
+__license__ = 'MIT'
 
 class lzlist(list):
-	def __init__(self, ip):
+	def __init__(self, ip=[]):
 		super().__init__(ip)
 		self.__copy = ip
 
 	def __hash__(self):
-		from hashlib import sha512
-		_h = sha512()
+		_h = _sha512()
 		[_h.update(str(i).encode('utf-32')) for i in self]
-		del sha512
 		return _h.hexdigest()
 
 	def includes_type(self,tp):
@@ -24,7 +34,7 @@ class lzlist(list):
 			return None
 
 	def revert(self):
-		super().__init__(dc(self.__copy))
+		super().__init__(_dc(self.__copy))
 		return self 
 	
 
@@ -37,7 +47,7 @@ class lzlist(list):
 
 	@property
 	def unique(self):
-		_tmp = dc(self)
+		_tmp = _dc(self)
 		_tmp.join_all()
 		return lzlist(set(_tmp))
 
@@ -52,31 +62,28 @@ class lzlist(list):
 		while _check():
 			_o = []
 			for i in self:
-				if type(i) == list:
+				if issubclass(type(i), list):
 					_o += i 
 				else:
 					_o.append(i)
-			super().__init__(dc(_o))
-			del _o
+			super().__init__(_dc(_o))
 		return self
 
 	def similar_to(self, sstr):
-		from re import search
 		_r = []
 		for i in self:
 			_t = type(i)
 			if _t == list:
 				continue
 			try:
-				_r.append(_t(search(f'^.*{str(sstr)}.*$', str(i)).group(0)))
+				_r.append(_t(_se(f'^.*{str(sstr)}.*$', str(i)).group(0)))
 			except AttributeError:
 				continue
-		del search
 		return lzlist(_r)
 
 	def split_mod(self, grps):
 		_gp = [[] for i in range(grps)]
-		_l = dc(self)
+		_l = _dc(self)
 		for count, i in enumerate(_l):
 			_gp[count%grps].append(i)
 		del _l
@@ -84,46 +91,37 @@ class lzlist(list):
 		return self
 
 	def split_to(self, grps):
-		from math import ceil as mceil
-		_g = mceil(len(self)/grps)
-		del mceil
+		_g = _mceil(len(self)/grps)
 		super().__init__([self[i:i+_g] for i in range(0,len(self),_g)])
 		return self
 
 	def deepshuffle(self,replace=True):
-		from random import shuffle
 		def _s(l):
-			shuffle(l)
+			_shuffle(l)
 			for i in l:
+				_shuffle(l)
 				if isinstance(i, list):
-					shuffle(i)
+					_shuffle(i)
 					_s(i)
+			_shuffle(l)
 			return l
 
 		if replace:
-			_k = _s(self)
+			_k = _s(_s(self))
 		else:
-			_k = dc(self)
-			_k = _s(_k)
-
-		del shuffle
+			_k = _dc(self)
+			_k = _s(_s(_k))
 		return lzlist(_k)
 
 	def shuffle(self,rp=False):
-		from random import shuffle
-		k = dc(self)
-		shuffle(k)
-		shuffle(k)
+		k = _dc(self)
+		[_shuffle(k) for i in lzint(_randrange(100))]
 		if rp:
-			super().__init__(dc(k))
-		del shuffle
+			super().__init__(_dc(k))
 		return lzlist(k)
 
 	def choice(self,amount):
-		from random import choices
-		_p =  choices(self, k=amount)
-		del choices
-		return lzlist(_p)
+		return lzlist(_sample((lzlist(self* (amount if amount>len(self) else 1)).join_all().shuffle()),amount))
 
 	def type_is_all(self, t):
 		return all([isinstance(i,t) for i in self])
@@ -144,16 +142,14 @@ class lzlist(list):
 		return len(self.all_with_type(t))
 
 	def forall(self,t='product'):
-		from itertools import product,permutations,combinations, combinations_with_replacement
 		if t.startswith('pr'):
-			_k = list([list(i) for i in product(self,repeat=2)])
+			_k = list([list(i) for i in _product(self,repeat=2)])
 		elif t.startswith('pe'):
-			_k = list([list(i) for i in permutations(self,2)])
+			_k = list([list(i) for i in _permutations(self,2)])
 		elif t.startswith('c') and t.endswith('s'):
-			_k = list([list(i) for i in combinations(self,2)])
+			_k = list([list(i) for i in _combinations(self,2)])
 		else:
-			_k = list([list(i) for i in combinations_with_replacement(self,2)])
-		del product, permutations, combinations, combinations_with_replacement
+			_k = list([list(i) for i in _combinations_with_replacement(self,2)])
 		return lzlist(_k)
 
 	@property
@@ -175,17 +171,23 @@ class lzlist(list):
 					yield self[0]
 		return _g(self, askval) if rt else lzlist(_g(self,askval))
 
-
-	def sort(self):
-		return sorted(self,key=lambda x: hash(x))
-
-
+	def run_all(self,*args,**kwargs):
+		try:
+			margs = list(args)[0]
+		except IndexError:
+			margs = []
+		try:
+			others= tuple(args[1:]) if len(args) > 1 else ()
+		except IndexError:
+			others= ()
+		if any([not callable(i) for i in self]):
+			return
+		return [self[i](*others, **kwargs) if i in margs else self[i]() for i in range(len(self))]
 
 	@staticmethod
 	def mkrandlist(length,start=None,end=None,sd=None):
-		from random import seed, randrange
 		if sd:
-			seed(sd)
+			_seed(sd)
 		if not start and end:
 			start=end
 			end= None
@@ -193,12 +195,11 @@ class lzlist(list):
 			start=100
 		_k=lzlist([])
 		for i in lzint(length):
-			seed(randrange(0,randrange(10000000000)))
+			_seed(_randrange(0,_randrange(10000000000)))
 			if start and end:
-				_k.append(randrange(start,end))
+				_k.append(_randrange(start,end))
 			else:
-				_k.append(randrange(start))
-		del seed, randrange
+				_k.append(_randrange(start))
 		return _k
 
 class lzstr(str):
@@ -219,7 +220,7 @@ class lzstr(str):
 
 	@property
 	def reversed(self):
-		return lzstr(dc(lzstr(''.join(reversed(list(self))))))
+		return lzstr(_dc(lzstr(''.join(reversed(list(self))))))
 
 	def remove(self,n):
 		from re import sub
@@ -234,8 +235,17 @@ class lzstr(str):
 	def split_by(self, end):
 		return [self[i:i+end] for i in range(0,len(self),end)]
 
-	def fillwith(self,tl,ends='0'):
-		return self+ends*(tl-len(self))
+	def fill(self,tl,ends='0'):
+		return self+str(ends)*(tl-len(self))
+
+	def shuffle(self):
+		_k=lzlist(self)
+		_k.shuffle(rp=True)
+		return ''.join(_k)
+
+	@staticmethod
+	def randstring(l):
+		return ''.join([_choice(_ascii_chars+_digs) for i in lzint(l)])
 
 
 class lzdict(dict):
@@ -250,7 +260,7 @@ class lzdict(dict):
 		del _k
 		return self
 
-	def get_fromvalue(self,val=None,gen=False):
+	def get_fromvalue(self,val=None):
 		if not val:
 			val= self.listvalues[0]
 
@@ -260,7 +270,7 @@ class lzdict(dict):
 					yield lzlist(self.keys())[count]
 			return None
 
-		return _s(self,val) if gen else lzlist(_s(self,val))
+		return lzlist(_s(self,val))
 
 	@property
 	def lists(self):
@@ -275,8 +285,29 @@ class lzdict(dict):
 		return lzlist(self.lists[1])
 
 	def randitems(self,i):
+		if i>len(self):
+			return self
 		return lzdict({k: v for k, v in lzlist(self.items()).choice(i)})
 
+	def find_inkeys(self,searchstring):
+		_r = lzlist()
+		for i in self.listkeys:
+			_t = type(i)
+			try:
+				_r.append(_t(_se(f'^.*{str(searchstring)}.*$',str(i)).group(0)))
+			except AttributeError:
+				continue
+		return lzdict({i:self[i] for i in _r})
+
+	def find_invalues(self,searchstring):
+		_r = lzlist()
+		for i in self.listvalues:
+			_t = type(i)
+			try:
+				_r.append(_t(_se(f'^.*{str(searchstring)}.*$',str(i)).group(0)))
+			except AttributeError:
+				continue
+		return {i:self[i] for i in lzlist([self.get_fromvalue(i) for i in _r]).join_all()}
 
 class lzint(int):
 	def __iter__(self):
@@ -328,9 +359,8 @@ class lzfloat(float):
 	def round_sf(self, l):
 		if l>self.digits_beforezero:
 			return round(self,l-self.digits_beforezero)
-		return lzint(lzstr(str(self)[:l]).fillwith(self.digits_beforezero))
+		print(10**(self.digits_beforezero-l))
 	
-
 
 
 
